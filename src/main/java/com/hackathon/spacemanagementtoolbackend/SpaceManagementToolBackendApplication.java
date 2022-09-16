4faf63949d23ffdd0d5ca7eaa17265d2509048e2
imrehan <rehanshaikh2156@gmail.com>
2022-09-16 21:59:47 +0530

    initial setup

---
+++ src/main/java/com/hackathon/spacemanagementtoolbackend/SpaceManagementToolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.spacemanagementtoolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceManagementToolBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceManagementToolBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hackathon/spacemanagementtoolbackend/SpaceManagementToolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.spacemanagementtoolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceManagementToolBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
